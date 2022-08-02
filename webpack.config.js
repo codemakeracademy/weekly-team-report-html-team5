cd0eb69a5fec5e1316018cab6dfc93b49655bdf8
Astemir Bekov <artisticheski@gmail.com>
2022-08-02 16:14:09 +0300

    setup project: webpack bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const isProduction = process.env.NODE_ENV === "production";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const stylesHandler = "style-loader";
+
+const pages = [
+  // Our 11 pages .html
+];
+
+
+
+const config = {
+  entry: pages.reduce((config, page) => {
+    config[page] = `./src/${page}.js`;
+    return config;
+  }, {}),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: '[name][ext]',
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+
+  plugins: [].concat(
+    pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          inject: true,
+          template: `./${page}.html`,
+          filename: `${page}.html`,
+          chunks: [page],
+        })
+    )
+  ),
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|svg|gif|woff|woff2|eot|jpg|ttf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+
+  return config;
+};
