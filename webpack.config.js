3e94e8ecef51e2471168e7ab97dfb0bbb982d2ac
Alexandra Melenteva <alexandram@Alexandras-MacBook-Air.local>
2022-08-10 15:44:25 +0300

    add fixes

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const stylesHandler = "style-loader";
 
 const pages = [
-  "index","team-reports", "my-reports","team-members", "weekly-report-history"
-  // Other html
+  "index",
+  "my-reports",
+  "team-reports",
+  "aside-bar",
+  "fill-out-report",
+  "sign-in",
+  "edit-profile",
+  "edit-member-info"
 ];
 
 
 const config = {
-  entry: pages.reduce((config, page) => {
-    config[page] = `./src/${page}.js`;
-    return config;
-  }, {}),
+  entry: "./src/index.js",
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, "dist"),
@@ -34,19 +37,49 @@ const config = {
     host: "localhost",
   },
 
-  plugins: [].concat(
-    pages.map(
-      (page) =>
-        new HtmlWebpackPlugin({
-          template: `./${page}.html`,
-          filename: `${page}.html`,
-          chunks: [page],
-          inject: true,
-        })
-    )
-  ),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./my-reports.html",
+      filename: "my-reports.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./fill-out-report.html",
+      filename: "fill-out-report.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./invite-your-team.html",
+      filename: "invite-your-team.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./team-reports.html",
+      filename: "team-reports.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./my-company.html",
+      filename: "my-company.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./sign-in.html",
+      filename: "sign-in.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./edit-profile.html",
+      filename: "./edit-profile"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./edit-member-info.html",
+      filename: "edit-member-info.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./weekly-report-history.html",
+      filename: "weekly-report-history.html"
+    }),
+  ],
   module: {
-
     rules: [
       {
         test: /\.s[ac]ss$/i,
@@ -64,6 +97,10 @@ const config = {
         test: /\.(png|svg|gif|woff|woff2|eot|jpg|ttf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
 };
@@ -78,3 +115,4 @@ module.exports = () => {
 
   return config;
 };
+
